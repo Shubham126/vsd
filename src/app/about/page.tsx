7c1c75bb831271be198d5918e9fdb27d365128e7
Subham <chromaticshubham26@gmail.com>
2025-10-01 21:48:55 +0530

    project setup

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1 className = "text-white">About Page</h1>
+      <p className = "text-white" >This is some information about the app or me.</p>
+    </div>
+  );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div>
+      <h1 className = "text-white">Contact Me Page</h1>
+      <p className = "text-white"> Reach out via email or form.</p>
+    </div>
+  );
+}
