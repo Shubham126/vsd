e0104cfed1720406dc9e8031e694268216ae6e27
Subham <chromaticshubham26@gmail.com>
2025-10-08 17:46:21 +0530

    phone number updated

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -9,7 +9,7 @@ export default function Introduction(){
     <div className="relative w-full h-screen overflow-hidden bg-black text-white">
       {/* Background Images */}
       
-      {/* Large Building - Your mobile style, my md+ style */}
+      {/* Large Building */}
       <div className="absolute top-16 left-5 w-[55%] h-[60%] md:top-10 md:left-5 md:w-[60%] md:h-[70%] lg:w-[55%] lg:h-[75%] xl:w-[50%] xl:h-[80%]">
         <Image
           src={i2}
@@ -19,7 +19,7 @@ export default function Introduction(){
         />
       </div>
 
-      {/* Triangle/Sky Image - Your mobile style, my md+ style */}
+      {/* Triangle/Sky Image  */}
       <div className="absolute top-4 right-4 w-[25%] h-[15%] md:w-[24%] md:h-[20%] lg:w-[22%] lg:h-[22%] xl:w-[20%] xl:h-[25%]">
         <Image
           src={i1}
@@ -29,7 +29,7 @@ export default function Introduction(){
         />
       </div>
 
-      {/* Worker Image - Your mobile style, my md+ style */}
+      {/* Worker Image */}
       <div className="absolute bottom-25 right-6 w-1/4 h-1/4 md:bottom-6 md:right-6 md:w-[30%] md:h-[30%] lg:w-[28%] lg:h-[35%] xl:w-[25%] xl:h-[38%]">
         <Image
           src={i3}
@@ -39,7 +39,7 @@ export default function Introduction(){
         />
       </div>
 
-      {/* Center Text - Your mobile positioning, my glass effect for all sizes */}
+      
       <div className="absolute top-50 left-20 sm:top-35 sm:left-60 md:inset-0 md:flex md:items-center md:justify-center p-4 z-10">
         <div className="bg-white/80 backdrop-blur-sm px-6 py-4 md:px-8 md:py-6 lg:px-10 lg:py-8 xl:px-12 xl:py-10 rounded-sm shadow-lg">
           <h1
@@ -53,12 +53,43 @@ export default function Introduction(){
         </div>
       </div>
 
-      {/* Contact Info - Your style for mobile, my enhanced for md+ */}
-      <div className="absolute bottom-30 md:bottom-2 left-4 md:left-10 text-sm md:text-lg lg:text-base space-y-1 text-white text-left z-10">
-        <p>+91 9599371996</p>
-        <p>+91 9718826667</p>
-        <p className="break-all md:break-normal">vsdengineeringenterprises@gmail.com</p>
+      {/* Contact Information Section */}
+      <div className="absolute bottom-15 md:bottom-2 left-4 md:left-10 text-sm md:text-lg lg:text-base space-y-2 text-white text-left z-10">
+      {/* Phone Numbers with Icon */}
+      <div className="flex items-center gap-3">
+        {/* Phone Icon SVG */}
+        <svg 
+          className="w-6 h-6 md:w-8 md:h-8 flex-shrink-0" 
+          viewBox="0 0 24 24" 
+          fill="none" 
+          stroke="currentColor" 
+          strokeWidth="2"
+        >
+          <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"/>
+        </svg>
+        <div className="flex flex-col">
+          <span>+91 9599371996</span>
+          <span>+91 9718826667</span>
+        </div>
       </div>
+
+      {/* Email with Icon */}
+      <div className="flex items-center gap-3">
+        {/* Email Icon SVG */}
+        <svg 
+          className="w-6 h-6 md:w-8 md:h-8 flex-shrink-0" 
+          viewBox="0 0 24 24" 
+          fill="none" 
+          stroke="currentColor" 
+          strokeWidth="2"
+        >
+          <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/>
+          <polyline points="22,6 12,13 2,6"/>
+        </svg>
+        <span className="break-all md:break-normal">vsdengineeringenterprises@gmail.com</span>
+      </div>
+    </div>
+
 
       {/* VSD Logo - Your style for mobile, my scaling for md+ */}
       <div className="fixed bottom-4 left-4 w-12 h-12 md:w-12 md:h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16 z-10">
